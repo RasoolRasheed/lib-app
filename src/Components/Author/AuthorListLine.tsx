e64d534a103019bbafa719db3b88a5f0284c95e3
rasool <rasool16681@gmail.com>
2021-05-27 00:25:53 +0530

    Author create responsive done

--- src/Components/Author/AuthorListLine.tsx
+++ src/Components/Author/AuthorListLine.tsx
@@ -1,27 +1,42 @@
 import {Col, Row} from "react-bootstrap";
 import {Edit, Trash, Trash2} from "react-feather";
 import './../../assets/styles/partials/AuthorListLine.scss';
+import React, {FC, useState} from "react";
+import AddAuthorModel from "./AddAuthorModal";
 
-const AuthorListLine : React.FC = () => {
+type authorsListLineProps = {
+    name: string,
+    id: number,
+    updateRequest: (id: number) => void,
+    delete: (id:number) => void;
+}
+
+const AuthorListLine : React.FC<authorsListLineProps> = (props) => {
+
+    const [isVisibleAddAuthorForm,setIsVisibleAddAuthorForm] = useState<boolean>(false);
+
+    const handleUpdateModel = () => {
+        setIsVisibleAddAuthorForm(true);
+    }
+
+    const handleCloseEvent = () => {
+        setIsVisibleAddAuthorForm(false);
+    }
+
+    const handleUpdateAuthorEvent = () => {
+        // return props.createAuthor(event, "");
+    }
+
     return(
-        //<ol>
         <li>
-            <li>
-            <Row>
-                <Col xs={10}>1.Author 01</Col>
-                <Col><Edit className="edit-btn" /></Col>
-                <Col><Trash2></Trash2></Col>
+            <Row className={"author-list-line"}>
+                <Col xs={10}>{props.id}. {props.name}</Col>
+                <Col className={"author-btn-container"}>
+                    <Edit className="edit-btn" onClick={() => props.updateRequest(props.id)}/>
+                    <Trash2 className="trash-btn" onClick={() => props.delete(props.id)}></Trash2>
+                </Col>
             </Row>
-            </li>
-            <li>
-                <Row>
-                    <Col xs={10}>1.Author 01</Col>
-                    <Col><Edit className="edit-btn" /></Col>
-                    <Col><Trash2></Trash2></Col>
-                </Row>
-            </li>
         </li>
-
     );
 }
 
