e0c7558e523da7c9cbb2065f7b928ebdb8f2fc2d
rasool <rasool16681@gmail.com>
2021-05-31 17:34:34 +0530

    Book Brush up

---
+++ src/Components/Author/Author.tsx
@@ -0,0 +1,135 @@
+import '../../assets/styles/Author.scss';
+import plusIcon from '../../assets/images/plus.svg';
+import crossIcon from '../../assets/images/x-circle.svg';
+import editIcon from '../../assets/images/edit.svg';
+import trashIcon from '../../assets/images/trash-2.svg';
+import AuthorListLine from "./AuthorListLine";
+import {Plus, XCircle} from "react-feather";
+import AddAuthorModel from "./AddAuthorModal";
+import IAuthor from "../../Interface/IAuthor";
+import React, {FC,useState} from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import UpdateAuthorForm from "./UpdateAuthorForm";
+
+type AuthorsProps = {
+    returnAvailableAuthors: (authors: IAuthor[]) => void
+};
+const Author: React.FC<AuthorsProps> = (props) => {
+
+    const [isVisibleAddAuthorForm,setIsVisibleAddAuthorForm] = useState<boolean>(false);
+    let authorId: number = 1;
+
+    const [authorsList,setAuthorsList] = useState<IAuthor[]>([]);
+    const [isVisibleUpdateAuthorForm,setIsVisibleUpdateAuthorForm] = useState<boolean>(false);
+    const [authorToBeUpdate, setAuthorToBeUpdate] = useState<number | null>(null);
+
+    const handleClickAddAuthorEvent = () => {
+        setIsVisibleAddAuthorForm(true);
+    }
+    const handleCreateAuthorEvent = (event:React.FormEvent,name:string) => {
+        event.preventDefault();
+        const newAuthor:IAuthor = {authorName:name};
+        const author = authorsList.slice();
+        author.push(newAuthor);
+        setIsVisibleAddAuthorForm(false);
+        setAuthorsList(author);
+        // Sending to books section
+        props.returnAvailableAuthors(author);
+    }
+
+    const handleClickCloseUpdateFormEvent = () => {
+        setIsVisibleUpdateAuthorForm(false);
+    }
+    const handleCloseEvent = () => {
+        setIsVisibleAddAuthorForm(false);
+    }
+
+    const handleUpdateAuthorRequestEvent = (id: number) => {
+            setIsVisibleUpdateAuthorForm(true);
+            setAuthorToBeUpdate(id);
+    }
+
+    const handleUpdateAuthorEvent = (event: React.FormEvent, name: string) =>
+    {
+        if (authorToBeUpdate === null) {
+            return;
+        }
+
+        const authors = authorsList.slice();
+        const newAuthor:IAuthor = {authorName:name};
+        authors.splice(authorToBeUpdate-1,1,newAuthor);
+        setAuthorsList(authors);
+        setAuthorToBeUpdate(null);
+        setIsVisibleUpdateAuthorForm(false);
+        event.preventDefault();
+        // Sending to books section
+        props.returnAvailableAuthors(authors);
+
+    }
+
+    const handleDeleteAuthorEvent = (id:number) =>
+    {
+        const authors: IAuthor[] = authorsList.slice();
+        authors.splice(id - 1, 1);
+        setAuthorsList(authors);
+        setIsVisibleUpdateAuthorForm(false);
+        // Sending to books section
+        props.returnAvailableAuthors(authors);
+    }
+    return(
+        <Container className="author-container px-5">
+            <Row>
+                <Col sm={12}>
+                    <Row>
+                        <Col><p className={"author-title"}>Authors</p></Col>
+                    </Row>
+                    <Row>
+                        {
+                            authorsList.length === 0 &&
+                            <Col><i>No authors added</i></Col>
+
+                        }
+                        <Col md={12}>
+                            <ul className="author-list px-0">
+                                {authorsList.map(
+                                    (author: IAuthor) => {
+                                        return (
+                                            <AuthorListLine
+                                                name={author.authorName}
+                                                id={authorId++}
+                                                key={authorId}
+                                                updateRequest={handleUpdateAuthorRequestEvent}
+                                                delete={handleDeleteAuthorEvent}/>
+                                        );
+                                    }
+                                )}
+                            </ul>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col className={"px-0 add-author-title"}>
+                            <Plus className={"add-author-plus"} onClick={ () => handleClickAddAuthorEvent()}></Plus> Add Author</Col>
+                    </Row>
+                </Col>
+                <Col className={"px-0"}>{
+                    isVisibleAddAuthorForm
+                    &&
+                    <AddAuthorModel closeForm={handleCloseEvent} createAuthor={handleCreateAuthorEvent}></AddAuthorModel>
+                    }
+                    {
+                        isVisibleUpdateAuthorForm
+                        &&
+                        <UpdateAuthorForm
+                            currentAuthorName={authorToBeUpdate ? authorsList[authorToBeUpdate - 1].authorName : ""}
+                            closeForm={handleClickCloseUpdateFormEvent}
+                            updateAuthor={handleUpdateAuthorEvent}
+                        />
+                    }
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Author;
---
+++ src/Components/Book/Book.tsx
@@ -0,0 +1,144 @@
+import React, {useState} from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import crossIcon from "../../assets/images/x-circle.svg";
+import { Plus, Trash2, Edit } from 'react-feather';
+import '../../assets/styles/Book.scss';
+import AddBookForm from '../Book/AddBookForm';
+import IAuthor from "../../Interface/IAuthor";
+import IBook from '../../Interface/IBook';
+import BookListLine from "./BookListLine";
+import UpdateBookForm from "./UpdateBookForm";
+import NoBooks from "./NoBooks";
+
+type BooksProps = {
+    authorsAvailable: () => IAuthor[]
+};
+
+const Books:React.FC<BooksProps> = (props) =>{
+    let bookId: number = 1;
+    const [isVisibleAddBookForm,setIsVisibleAddBookForm] = useState<boolean>(false);
+    const [bookList,setBookList] = useState<IBook[]>([]);
+    // Visibility of 'UpdateBook' form
+    const [isVisibleUpdateBookForm, setIsVisibleUpdateBookForm] = useState<boolean>(false);
+    // Book to be update
+    const [bookToBeUpdate, setBookToBeUpdate] = useState<number | null>(null);
+
+    const handleCloseEvent = () => {
+        setIsVisibleAddBookForm(false);
+    }
+
+    const handleAddBookForm = () => {
+        setIsVisibleAddBookForm(true);
+    }
+
+    const handleCreateBook = (event:React.FormEvent,name:string,price:string,author:string) => {
+        event.preventDefault();
+        const newBook:IBook = {bookName:name,bookPrice:price,bookAuthor:author};
+        const book = bookList.slice();
+        book.push(newBook);
+        setBookList(book);
+        console.log(book);
+        setIsVisibleAddBookForm(false);
+    }
+
+    // Update a 'Book'
+    const handleUpdateBookRequestEvent = (id: number) => {
+
+        setIsVisibleAddBookForm(false);
+        setIsVisibleUpdateBookForm(true);
+        setBookToBeUpdate(id);
+    }
+
+    // Set 'UpdateBook' form invisible
+    const handleClickCloseUpdateFormEvent = () => {
+        setIsVisibleUpdateBookForm(false);
+    }
+    // Delete a 'Book'
+    const handleDeleteBookEvent = (id: number) => {
+        const books: IBook[] = bookList.slice();
+        books.splice(id - 1, 1);
+        setBookList(books);
+        setIsVisibleUpdateBookForm(false);
+    }
+
+    const handleUpdateBookEvent = (event: React.FormEvent, title: string, price: string, author: string) => {
+        if (bookToBeUpdate === null) {
+            return;
+        }
+
+        const books = bookList.slice();
+        const newBook: IBook = {bookName: title, bookPrice: price, bookAuthor: author};
+        books.splice(bookToBeUpdate - 1, 1, newBook);
+        setBookList(books);
+        setBookToBeUpdate(null);
+        setIsVisibleUpdateBookForm(false);
+        event.preventDefault();
+    }
+
+    return(
+        <Container className={"px-5"}>
+            <Row className="Books px-0">
+                <Col sm={12} md={12}>
+                    <h3 className="books-title">Books</h3>
+                </Col>
+                {
+                    bookList.length===0 && <NoBooks></NoBooks>
+                }
+                <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                    <ul className="book-list px-0">
+                        {bookList.map(
+                            (book: IBook) => {
+                                return (
+                                    <BookListLine
+                                        title={book.bookName}
+                                        price={book.bookPrice}
+                                        author={book.bookAuthor}
+                                        id={bookId++}
+                                        key={bookId}
+                                        delete={handleDeleteBookEvent}
+                                        updateRequest={handleUpdateBookRequestEvent}
+                                    />
+                                );
+                            }
+                        )}
+                    </ul>
+                </Col>
+                <Col sm={12} md={12}>
+                    {/*<span className="no-books"><i>No books listed here</i></span>*/}
+                    <p className="add-book pt-3">
+                        <Plus color="#034fa5" className="add-book-plus" onClick={handleAddBookForm}/>
+                        <span className="add-book-text">
+                    Add Book
+                </span>
+                    </p>
+                </Col>
+                <Col>
+                    {
+                        isVisibleAddBookForm
+                        &&
+                        <AddBookForm
+                            closeForm={handleCloseEvent}
+                            createBook={handleCreateBook}
+                            authors={props.authorsAvailable}
+                        />
+                    }
+                    {
+                        isVisibleUpdateBookForm
+                        &&
+                        <UpdateBookForm
+                            currentTitle={bookToBeUpdate === null ? "" : bookList[bookToBeUpdate - 1].bookName}
+                            currentPrice={bookToBeUpdate === null ? "" : bookList[bookToBeUpdate - 1].bookPrice}
+                            currentAuthor={bookToBeUpdate === null ? "" : bookList[bookToBeUpdate - 1].bookAuthor}
+                            closeForm={handleClickCloseUpdateFormEvent}
+                            updateBook={handleUpdateBookEvent}
+                            authors={props.authorsAvailable}
+                        />
+                    }
+                </Col>
+
+            </Row>
+        </Container>
+    );
+}
+
+export default Books;
